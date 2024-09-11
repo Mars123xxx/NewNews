137fe158d60200a4553cc77e4595d3abe83a5ca4
Mars <2836635695@qq.com>
2024-09-11 22:21:33 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base:"./"
+  base:"./",
+  server:{
+    port: 80,
+    host: '0.0.0.0'
+  }
 })
